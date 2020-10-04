87e0003434960cd11c75700327d18b7fd5462cf4
Jeferson Silva <jefersoun.silva@gmail.com>
2020-10-04 18:04:38 -0300

    logical service finish

---
+++ backend/src/main/java/com/example/tijolao/model/Letter.java
@@ -0,0 +1,14 @@
+package com.example.tijolao.model;
+
+public class Letter {
+    public String key;
+    public String[] letters;
+
+
+    public Letter(String key, String[]letters) {
+        this.key = key;
+        this.letters = letters;
+    }
+}
+
+
--- backend/src/main/java/com/example/tijolao/service/TijolaoService.java
+++ backend/src/main/java/com/example/tijolao/service/TijolaoService.java
@@ -1,11 +1,22 @@
 package com.example.tijolao.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.tijolao.util.Dictionary;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TijolaoService {
 
 	public String getMessage(String key, int count) {
-		return "teste";
-	}
+        String[] letters  = getLetter(key);
+        return letters[count-1];
+    }
+    
+    private String[] getLetter(String key) {
+        Dictionary dictionary = new Dictionary();
+        return dictionary.getLetters(key);
+    }
 }
