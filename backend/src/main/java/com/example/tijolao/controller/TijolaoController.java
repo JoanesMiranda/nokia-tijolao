47e548106479e17f6a0d43b21693f5ffc217f6ac
Jeferson Silva <jefersoun.silva@gmail.com>
2020-10-04 16:47:27 -0300

    service initial

--- backend/src/main/java/com/example/tijolao/controller/TijolaoController.java
+++ backend/src/main/java/com/example/tijolao/controller/TijolaoController.java
@@ -1,5 +1,8 @@
 package com.example.tijolao.controller;
 
+import com.example.tijolao.service.TijolaoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tijolao")
 public class TijolaoController {
+
+    @Autowired
+    private TijolaoService service;
     
     @GetMapping
     public String getMessage(@RequestParam int codigo){
-        return "test";
+        return service.getMessage(codigo);
     }
     
 }
---
+++ backend/src/main/java/com/example/tijolao/service/TijolaoService.java
@@ -0,0 +1,11 @@
+package com.example.tijolao.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TijolaoService {
+
+    public String getMessage(int codigo){
+        return "message";
+    }
+}
