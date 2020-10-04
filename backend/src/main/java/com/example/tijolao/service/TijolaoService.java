91051397cc21fd574448f6e60c79242034a1c8cb
Jeferson Silva <jefersoun.silva@gmail.com>
2020-10-04 18:33:04 -0300

    resolve requests

--- backend/src/main/java/com/example/tijolao/service/TijolaoService.java
+++ backend/src/main/java/com/example/tijolao/service/TijolaoService.java
@@ -1,8 +1,5 @@
 package com.example.tijolao.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.example.tijolao.util.Dictionary;
 
 import org.springframework.stereotype.Service;
@@ -10,11 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class TijolaoService {
 
-	public String getMessage(String key, int count) {
-        String[] letters  = getLetters(key);
-        return letters[count-1];
+    public String getMessage(String key, int count) {
+        if (key.equals("1")) {
+            return null;
+        }
+        return key.equals("0") ? getLetters(key)[0] : getLetters(key)[count - 1];
+
     }
-    
+
     private String[] getLetters(String key) {
         Dictionary dictionary = new Dictionary();
         return dictionary.getLetters(key);
