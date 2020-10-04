e3aa23db2f4b285415067645b868642076535116
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-04 18:22:01 -0300

    Posicionamento de fragmentos

--- frontend/src/pages/nokia/index.tsx
+++ frontend/src/pages/nokia/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Container } from './styles';
 
-import base from '../../assets/base.png';
 import bt0 from '../../assets/bt0.png';
 import bt1 from '../../assets/bt1.png';
 import bt2 from '../../assets/bt2.png';
@@ -23,21 +22,21 @@ const Nokia: React.FC = () => {
     return (
         <>
         <Container>
-           <img src={base} alt="Github Explorer" />
-           <button><img src={bt0}/> </button>
-           <button><img src={bt1}/> </button>
-           <button><img src={bt2}/> </button>
-           <button><img src={bt3}/> </button>
-           <button><img src={bt4}/> </button>
-           <button><img src={bt5}/> </button>
-           <button><img src={bt6}/> </button>
-           <button><img src={bt7}/> </button>
-           <button><img src={bt8}/> </button>
-           <button><img src={bt9}/> </button>
-           <button><img src={btAst}/> </button>
-           <button><img src={btHash}/> </button>
-           <button><img src={enter}/> </button>
-
+            <button><img src={enter}/> </button>
+            <div className="teclas">
+                <button><img src={bt1}/> </button>
+                <button><img src={bt2}/> </button>
+                <button><img src={bt3}/> </button>
+                <button><img src={bt4}/> </button>
+                <button><img src={bt5}/> </button>
+                <button><img src={bt6}/> </button>
+                <button><img src={bt7}/> </button>
+                <button><img src={bt8}/> </button>
+                <button><img src={bt9}/> </button>
+                <button><img src={btAst}/> </button>
+                <button><img src={bt0}/> </button>
+                <button><img src={btHash}/> </button>
+            </div>
         </Container>
         </>
     )
--- frontend/src/pages/nokia/styles.ts
+++ frontend/src/pages/nokia/styles.ts
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
+import base from '../../assets/base.png';
+export const Container = styled.div`
 
-export const Container = styled.header`
+  background: url(${base}) no-repeat;
+  background-size:334px;
+  width: 334px;
+  height: 750px;
 
+  .teclas {
+    padding-top: 490px;
+    padding-left: 45px;
+    width: 334px;
+    display: grid;
+    grid-column-gap: 43px;
+    grid-row-gap: 10px;
+    grid-template-columns: repeat(3, 43px);
+
+    button{
+      width: 68px;
+      background: none;
+      color: inherit;
+      border: none;
+      padding: 0;
+      font: inherit;
+      cursor: pointer;
+      outline: inherit;
+
+      img{
+        width: 68px;
+      }
+
+    }
+  }  
 `;
